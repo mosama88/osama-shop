69563343fb0f2df3b585453c01f3c7638c383388
mosama88 <mosama88@hotmail.com>
2024-04-04 13:33:56 +0200

    update Pro

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -19,6 +19,10 @@ class ProfileUpdateRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'mobile' => ['required', 'string', 'min:10', 'max:18'],
+            'country' => ['required', 'string', 'min:3', 'max:20'],
+            'bd' => ['nullable', 'date', 'before_or_equal:today', 'date_format:Y-m-d'],
+            'gender' => ['nullable', 'string', Rule::in(['male', 'female'])],
         ];
+
     }
 }
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -9,14 +9,14 @@
         </p>
     </header>
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+    <form id="send-verification" method="post" action="{{ route('verification.send') }}" enctype="multipart/form-data">
         @csrf
     </form>
 
     <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('patch')
-
+        {{-- Name Input --}}
         <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)"
@@ -24,6 +24,7 @@
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
+        {{-- Email Input --}}
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)"
@@ -52,13 +53,59 @@
             @endif
 
         </div>
+
+        {{-- Mobile Input --}}
         <div>
             <x-input-label for="mobile" :value="__('Mobile')" />
             <x-text-input id="mobile" name="mobile" type="text" class="mt-1 block w-full" :value="old('mobile', $user->mobile)"
                 required autofocus autocomplete="mobile" />
             <x-input-error class="mt-2" :messages="$errors->get('mobile')" />
         </div>
-        <div class="flex items-center gap-4">
+
+        {{-- Country Input --}}
+        <div>
+            <x-input-label for="mobile" :value="__('Country')" />
+            <x-text-input id="mobile" name="country" type="text" class="mt-1 block w-full" :value="old('country', $user->country)"
+                required autofocus autocomplete="country" />
+            <x-input-error class="mt-2" :messages="$errors->get('country')" />
+        </div>
+
+        {{-- Country Input --}}
+        <div>
+            <x-input-label for="mobile" :value="__('Gender')" />
+            <select
+                class="form-select border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                name="gender" aria-label="Default select example">
+                <option selected="" disabled>أختر من القائمة</option>
+                <option value="male"{{ $user->gender === 'male' ? 'selected' : '' }}>Male</option>
+                <option value="female"{{ $user->gender === 'female' ? 'selected' : '' }}>Female</option>
+            </select>
+            @error('gender')
+                <div class="mt-2" :messages="$errors - > get('gender')">{{ $message }}</div>
+            @enderror
+        </div>
+
+
+        {{-- Birthday Input --}}
+        <div>
+            <form method="post" action="{{ route('profile.update') }}">
+                <x-input-label for="birthday" :value="__('Birthday')" />
+                <input type="date"
+                    class="form-select border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                    id="birthday" name="birthday">
+                <input type="submit">
+            </form>
+        </div>
+
+        {{-- Image Input --}}
+        <div class="mt-3">
+            <x-input-label for="image" :value="__('Image')" />
+            <x-text-input id="image" name="image" type="file" class="mt-1 block w-full" :value="old('image', $user->country)"
+                required autofocus autocomplete="country" />
+            <x-input-error class="mt-2" :messages="$errors->get('image')" />
+        </div>
+
+        <div class="flex items-center gap-4 mt-3">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
